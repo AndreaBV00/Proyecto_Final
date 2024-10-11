1a8db9248c5794e629fbc763d543c869becf829e
AndreaBV00 <azulaitami@gmail.com>
2024-10-11 20:16:30 -0300

    admin

--- App_Artista/admin.py
+++ App_Artista/admin.py
@@ -1,3 +1,11 @@
 from django.contrib import admin
+from .models import Artista_invitado, Artista, Obra, Exposicion 
 
 # Register your models here.
+admin.register(Artista_invitado)
+
+admin.register(Artista)
+
+admin.register(Obra)
+
+admin.register(Exposicion)
